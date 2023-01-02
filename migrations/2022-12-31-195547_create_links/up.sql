7d2b9e8d0d529d6dd13e27ede1b1946811c81dee
Bang Koboi <TheManHimself@jimmyganteng.com>
2023-01-02 23:04:07 +0700

    removing migrations folder

--- migrations/2022-12-31-195547_create_links/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE "links" (
-	"shortUrl"	TEXT NOT NULL UNIQUE,
-	"redirectUrl"	TEXT,
-	"photoUrl"	TEXT,
-	"title"	TEXT,
-	"description"	TEXT,
-	PRIMARY KEY("shortUrl")
-);
